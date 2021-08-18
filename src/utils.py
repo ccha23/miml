51a2dd321f4fbdb974221a97f212003e152e9561
wangxuuu <wangxucoco@live.com>
2021-08-18 16:26:35 +0800

    add trained models

--- src/utils.py
+++ src/utils.py
@@ -12,9 +12,16 @@ import itertools
 from tqdm import tqdm
 
 cuda = True if torch.cuda.is_available() else False
+device = torch.device("cuda" if cuda else "cpu")
 FloatTensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
 LongTensor = torch.cuda.LongTensor if cuda else torch.LongTensor
 
+def gaussian_data(rho=0.9, mean=[0, 0],sample_size=100):
+    cov = np.array([[1, rho], [rho, 1]])
+    return np.random.multivariate_normal(
+        mean=mean,
+        cov=cov,
+        size=sample_size)
 
 # Build a neural network
 class Net(nn.Module):
@@ -62,6 +69,13 @@ def div(net, data, ref):
     log_mean_ef_ref = torch.logsumexp(net(ref), 0) - np.log(ref.shape[0])
     return mean_f - log_mean_ef_ref
 
+def _resample(data, batch_size, replace=False):
+    # Resample the given data sample.
+    index = np.random.choice(
+        range(data.shape[0]), size=batch_size, replace=replace)
+    batch = data[index]
+    return batch
+
 class Generator(nn.Module):
     def __init__(self, dim, hidden_dim, y_dim, sigma=0.02):
         super(Generator, self).__init__()
@@ -103,3 +117,133 @@ class Discriminator(nn.Module):
         output = F.elu(self.fc2(output))
         output = self.fc3(output)
         return output
+
+
+def train_gan_minee(args, X, Y):
+    # Initialize generator and discriminator
+    generator1 = Generator(dim=args.d, hidden_dim=args.hidden_dim, y_dim=args.d)
+    generator2 = Generator(dim=args.d, hidden_dim=args.hidden_dim, y_dim=args.d)
+    # discriminator = Net(input_size=d*2, hidden_size=100)
+    discriminator = Discriminator(input_size=args.d*2, hidden_size=100)
+
+    if args.load_available and os.path.exists(args.chkpt_name):
+        checkpoint = torch.load(args.chkpt_name, map_location='cuda' if torch.cuda.is_available() else 'cpu')
+        mi_list = checkpoint['mi_list']
+        discriminator.load_state_dict(checkpoint['discriminator'])
+        generator1.load_state_dict(checkpoint['generator1'])
+        generator2.load_state_dict(checkpoint['generator2'])
+    else:
+        mi_list = []
+        if cuda:
+            generator1.cuda()
+            generator2.cuda()
+            discriminator.cuda()
+
+    
+    # Optimizers Adam
+    optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+    optimizer_G = torch.optim.Adam(
+        itertools.chain(generator1.parameters(), generator2.parameters()), lr=args.lr, betas=(args.b1, args.b2)
+    )
+
+    for i in tqdm(range(args.n_iters)):
+        z = FloatTensor(np.random.normal(0, 1, (args.sample_size*args.ref_batch_factor, args.d)))
+
+        y_gen = generator1(z)
+        x_gen = generator2(z)
+
+        XY = torch.cat((X, Y), dim=1)
+        y_gen_ref = _resample(y_gen, batch_size=args.batch_size*args.ref_batch_factor)
+        x_gen_ref = _resample(x_gen, batch_size=args.batch_size*args.ref_batch_factor)
+
+        batch_XY = _resample(XY, batch_size=args.batch_size)
+        batch_XY_gen_ref = torch.cat((x_gen_ref,y_gen_ref), dim=1)
+        # -----------------
+        #  Train Generator
+        # -----------------
+        optimizer_G.zero_grad()
+        log_mean_ef_ref = torch.logsumexp(discriminator(batch_XY_gen_ref), 0) - np.log(batch_XY_gen_ref.shape[0])
+        gen_loss = -log_mean_ef_ref
+        gen_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+        # define the loss function with moving average in the gradient estimate
+        for _ in range(2):
+            z = FloatTensor(np.random.normal(0, 1, (args.sample_size*args.ref_batch_factor, args.d)))
+
+            y_gen = generator1(z)
+            x_gen = generator2(z)
+
+            XY = torch.cat((X, Y), dim=1)
+            y_gen_ref = _resample(y_gen, batch_size=args.batch_size*args.ref_batch_factor)
+            x_gen_ref = _resample(x_gen, batch_size=args.batch_size*args.ref_batch_factor)
+
+            batch_XY = _resample(XY, batch_size=args.batch_size)
+            batch_XY_gen_ref = torch.cat((x_gen_ref,y_gen_ref), dim=1)
+
+            optimizer_D.zero_grad()
+            mean_fXY = discriminator(batch_XY).mean()
+            mean_efXY_ref = torch.exp(discriminator(batch_XY_gen_ref)).mean()
+            args.ma_ef = (1-args.ma_rate)*args.ma_ef + args.ma_rate*mean_efXY_ref
+            batch_loss_XY = - mean_fXY + (1 / args.ma_ef).detach() * mean_efXY_ref
+            batch_loss_XY.backward()
+            optimizer_D.step()
+
+        mi_list.append(div(discriminator, XY, batch_XY_gen_ref).cpu().item())
+    torch.save({
+        'mi_list': mi_list,
+        'discriminator': discriminator.state_dict(),
+        'generator1': generator1.state_dict(), 
+        'generator2': generator2.state_dict()
+    }, args.chkpt_name)
+    
+    # torch.save(discriminator.state_dict(), args.chkpt_name_disc)
+    # torch.save(generator1.state_dict(), args.chkpt_name_gen1)
+    # torch.save(generator2.state_dict(), args.chkpt_name_gen2)
+    return mi_list
+
+def smooth_ce_loss(pre_label, true_label, smoothing, num_classes):
+    new_labels = (1.0 - smoothing) * true_label + smoothing / num_classes
+    return torch.nn.BCELoss()(pre_label, new_labels)
+
+def acti_func(x, a, b, c):
+    # a is \alpha_0, b is \tau and c is 1-\tau in the paper
+    alpha = torch.zeros_like(x)
+    x_cpu = x.cpu()
+    alpha[np.where(x_cpu.cpu()<=b)] = - a*x[np.where(x_cpu<=b)]/b + a
+    alpha[np.where((x_cpu>b) & (x_cpu<c))] = 0
+    alpha[np.where(x_cpu>=c)] = a*x[np.where(x_cpu>=c)]/(1-c) + a*c/(c-1)
+    return alpha
+
+def train_classifier(args, X, Y, loss_type):
+    model = Net(input_size=X.shape[1], hidden_size=args.hidden_size)
+    if args.load_available and os.path.exists(args.chkpt_name):
+        model.load_state_dict(torch.load(args.chkpt_name))
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, betas=(args.b1, args.b2))
+    if cuda:
+        model.to(device)
+    # cross-entropy loss
+    for i in range(args.niters):
+        batch_X, batch_Y = resample(X, Y, batch_size=args.batch_size, replace=True)
+        optimizer.zero_grad()
+        pred_Y = model(batch_X)
+        acc = accuracy(batch_Y.reshape(-1,1), pred_Y)
+        if loss_type == "ce_loss":
+            loss = torch.nn.BCELoss()(pred_Y, batch_Y.reshape(-1,1))
+        elif loss_type == "ls_loss":
+            loss = smooth_ce_loss(pred_Y, batch_Y.reshape(-1,1), 0.1, 2)
+        elif loss_type == "AdapLS_loss":
+            a, b, c = 0.01, 1e-3, 1-1e-3
+            conf = acti_func(pred_Y, a, b, c)
+            loss = smooth_ce_loss(pred_Y, batch_Y.reshape(-1,1), conf.detach(), 2)
+        else:
+            raise NotImplementedError
+        loss.backward()
+        optimizer.step()
+        if i%1000==0:
+            print("Iternation: %d, loss: %f, acc: %f"%(i, loss.item(), acc))
+    torch.save(model.state_dict(), args.chkpt_name)
+    return model
